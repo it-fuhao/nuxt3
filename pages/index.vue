8f5c7a915201b6fe8cc8c4356a9bff1e970f5502
fuhao <fuhao1px@163.com>
2021-10-13 17:01:53 +0800

    路由跳转

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
     这是主体内容
+    <button @click="handleRouter">跳转</button>
   </div>
 </template>
-<script>
-  export default {
-    layout: 'custom', // 使用layouts布局
+<script lang="ts">
+import { defineComponent, reactive, toRefs, computed } from 'vue';
+import { useRouter } from 'vue-router';
+export default defineComponent({
+  layout: 'custom', // 使用layouts布局
+  setup() {
+    const router = useRouter();
+    // 路由跳转
+    const handleRouter = () => {
+      router.push({ name: 'product' });
+    }
+    return {
+      handleRouter
+    }
   }
-</script>
+})
+</script> 
