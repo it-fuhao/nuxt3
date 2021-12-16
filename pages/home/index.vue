0b5812f8cd4f83753a348ca29e172fc9c2c480d0
fuhao1px <fuhao1px@163.com>
2021-12-16 20:39:46 +0800

    动态组件

---
+++ pages/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <component :is="functional" />
+</template>
+<script lang="ts" setup>
+
+const route = useRoute();
+const { componentName } = route.query;
+
+const functional = shallowRef();
+
+onBeforeMount(async () => {
+  const components = await import(`../../functional/${componentName}.vue`);
+  functional.value = components.default;
+});
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,50 @@
 <template>
   <div>
-    这是主体内容
     <van-button type="primary" @click="handleRouter">
-      跳转
+      跳转测试页面
     </van-button>
+    <van-cell
+      v-for="(functional, index) in list"
+      :key="index"
+      :title="functional.title"
+      is-link
+      @click="handleFunctional(functional)"
+    />
   </div>
 </template>
+<script lang="ts" setup>
+
+  interface IFunctional {
+    id: number;
+    title: string;
+    componentName: string;
+  }
+
+const router = useRouter();
+
+const list = ref<IFunctional[]>([
+  { id: 1, title: '动态组件测试', componentName: 'parent-child-components' },
+]);
+
+const handleFunctional = (functional: IFunctional) => {
+  router.push({
+    name: 'home',
+    query: {
+      componentName: functional.componentName
+    }
+  });
+};
+  // 路由跳转
+const handleRouter = () => {
+  router.push({ name: 'test' });
+};
+</script>
 <script lang="ts">
 export default defineNuxtComponent({
   layout: 'custom', // 使用layouts布局
   head () {
     return {
       title: 'nuxt3 demo 首页'
-    };
-  },
-  asyncData () {
-    // console.log(process);
-  },
-  setup () {
-    const router = useRouter();
-    // const aaaa = '121555';
-
-    // 路由跳转
-    const handleRouter = () => {
-      router.push({ name: 'test' });
-    };
-    return {
-      handleRouter
     };
   }
 });
